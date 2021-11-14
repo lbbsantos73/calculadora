11d145afbdeb38a670c7923341493cd85fcaf89a
luciano <luciano.santos@mac.com>
2021-11-14 08:44:52 -0300

    porcentagem

--- App.js
+++ App.js
@@ -30,6 +30,9 @@ export default function App() {
       case '/': 
         setCurrentNumber((fistNumber / lastNumber).toString())
         return
+        case '%': 
+        setCurrentNumber((fistNumber * lastNumber / 100).toString())
+        return
     }
   }
 
